a6989f60ecf7c9c0955703b24bd49a24ca0bf837
WordlessSoda <WordlessSoda@users.noreply.github.com>
2018-09-24 17:15:29 +0800

    Revert "Strat"
    This reverts commit 0da9e26fce0e75045996e3dfb1631d7c8af2f025.

--- files/page1/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
-var b="url",c="page1.html",d="generationDate",e=new Date(1537778843467.68),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="3737fe2015fd4f4fa1264d63a82495db",q="type",r="Axure:Page",s="name",t="page1",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="81a04c81b0474cb8841140ba8248a967",Y="label",Z="friendlyType",ba="图片",bb="imageBox",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=335,bi="height",bj=598,bk="75a91ee5b9d042cfa01b8d565fe289c0",bl="imageOverrides",bm="images",bn="normal~",bo="images/page1/u20.png",bp="masters",bq="objectPaths",br="81a04c81b0474cb8841140ba8248a967",bs="scriptId",bt="u20";
-return _creator();
-})());
